727ab9c3a6eb61e2e86c6f71e6b8ca19bcf2fbba
WrBug <i@wrbug.com>
2022-01-18 11:00:11 +0800

    :sparkles:  migrate to androidx

--- app/src/main/java/com/wrbug/kv/sample/MainActivity.kt
+++ app/src/main/java/com/wrbug/kv/sample/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.wrbug.kv.sample
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
--- test_module/build.gradle.kts
+++ test_module/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 val kotlin_version: String by project.rootProject.extra
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
-    implementation("com.android.support:appcompat-v7:28.0.0")
+    implementation("androidx.appcompat:appcompat:1.0.0")
     implementation(project(":kv"))
     kapt(project(":kv-compile"))
 }
